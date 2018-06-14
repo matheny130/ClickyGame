5c02df9d3659ceed1c3342614c60a417fa1166af
Mike Matheny <mike.matheny.81@gmail.com>
2018-06-14 00:20:21 -0400

    final commit

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
   <header className="header">
-    <h1>Kitty Clicky Game!</h1>
+    <h1>Kitty Clicky Game</h1>
     <h2>Click on any kitty to earn a point but don't click on the same kitty twice! Click all 12 kitties and you win.</h2>
   </header>
 );
